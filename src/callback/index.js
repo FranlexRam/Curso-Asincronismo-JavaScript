d875347bd26099ac9a4f4a46d24c773ea3b759ca
Franlex Ramirez <franlex.ramirez@gmail.com>
2023-01-28 10:15:11 -0400

    Callbacks

---
+++ src/callback/index.js
@@ -0,0 +1,22 @@
+// Se tiene como objetivo sumar dos valores.
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+function calc(num1, num2, callback) {
+   return callback(num1, num2) 
+}
+
+console.log(calc(2, 3, sum));
+
+
+//Aprendiendo a usar el setTimeOut
+setTimeout (function(){
+    console.log('Hola JavaScript');
+}, 5000);
+
+function greeting(name) {
+    console.log(`Hola ${name}`);
+}
+
+setTimeout(greeting, 2000, 'Franlex');
