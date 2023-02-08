1747cc99309b996696ba68947639014c1f59ca0e
Franlex Ramirez <franlex.ramirez@gmail.com>
2023-02-07 21:08:46 -0400

    async & await

--- src/async/index.js
+++ src/async/index.js
@@ -1,1 +1,19 @@
 //Construir la logica de como funciona async y await dentro de JS
+
+const fnAsync = () => {
+    return new Promise((resolve, reject) =>{
+        (true)
+        ? setTimeout(() => resolve('Async!!'), 2000)
+        : reject(new Error('Error!'));
+    })
+}
+
+const anotherFn = async () => {
+    const something = await fnAsync();
+    console.log(something);
+    console.log('Hello');
+}
+
+console.log('Before');
+anotherFn();
+console.log('after');
